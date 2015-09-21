413a6f8db8f0939568ed1fc56fed9c2023daa70a
Aaron Norby <aaron.norby@gmail.com>
2015-09-21 15:59:50 -0700

    Add d3 code to directive

--- public/app/controllers/d3Controller.js
+++ public/app/controllers/d3Controller.js
@@ -1,4 +1,21 @@
 angular.module('riskField.controllers')
   .controller('d3ArrayController', ['$scope', function($scope) {
-    
+
+     // test data;
+     var firstAlternative = Math.random();
+     var secondAlternative = 1 - firstAlternative;
+
+     var height = '500';
+     var width = '500';
+
+     var riskData = [];
+     for (var i = 0; i < Math.floor(100*firstAlternative); i++) {
+       riskData.push('first');
+     }
+     for (var i = 0; i < Math.ceil(100*secondAlternative); i++) {
+       riskData.push('second');
+     }
+
+     $scope.riskData = _.shuffle(riskData);
+
   }]);
--- public/app/directives/d3Array.js
+++ public/app/directives/d3Array.js
@@ -4,11 +4,43 @@ angular.module('riskField.directives')
       // restrict use of the directive to element tags
       restrict: 'E',
 
+      scope: {
+        data: '='
+      },
+
       link: function(scope, element, attributes) {
+        var width = 500;
+        var height = 500;
+
         // attaching to the directive's element
         // element is wrapped in jqlite so it's array-like
         var svg = d3.select(element[0])
-          .append('svg');
+          .append('svg')
+          .attr('height', height)
+          .attr('width', width);
+
+        svg.selectAll('circle')
+          .data(scope.data)
+          .enter()
+          .append('svg:circle')
+          .attr('cx', 250)
+          .attr('cy', -90)
+          .transition()
+          .ease('square')
+          .duration(400)
+          .delay(function(d, i) {return i * 50;})
+          .attr('cx', function(d, i) {
+            return (i * 50)%500 + 9;
+          })
+          .attr('cy', function(d, i) {
+            return (Math.floor(i/10)) * 50 + 15;
+          })
+          .attr('r', 9)
+          .attr('fill', function(d) {
+            return colors[d];
+          });
+            
+            
       }
     };
   }]);
