53431add261a690c0a6c9dbb346517f883145907
Aaron Norby <aaron.norby@gmail.com>
2015-09-21 15:19:47 -0700

    Define main app module and fill out dependencies

---
+++ public/app/riskField.js
@@ -0,0 +1,8 @@
+angular.module('riskField', [
+    'riskField.directives',
+    'riskField.controllers'
+]);
+
+angular.module('d3', []);
+angular.module('riskField.directives', ['d3']);
+angular.module('riskField.controllers', []);
